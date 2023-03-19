50199990b65dec89db9678a9d89b2050d8d74c78
Isaac Iniongun <iniongunisaac@gmail.com>
2023-03-19 21:01:27 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Isaac Iniongun on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
