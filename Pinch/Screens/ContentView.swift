184b480d96a8bbe62c8df6fb18ddbb578127fcfb
Isaac Iniongun <iniongunisaac@gmail.com>
2023-03-20 21:29:32 +0100

    add magnification gesture and zoom controls

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
     
     func resetImageState() {
         withAnimation(.spring()) {
@@ -31,8 +32,9 @@ struct ContentView: View {
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
                     .animation(.linear(duration: 1), value: isAnimating)
-                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .offset(imageOffset)
                     .scaleEffect(imageScale)
+                    //MARK: Double Tap
                     .onTapGesture(count: 2) {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
@@ -42,6 +44,7 @@ struct ContentView: View {
                             resetImageState()
                         }
                     }
+                    //MARK: Drag Gesture
                     .gesture(
                         DragGesture()
                             .onChanged { value in
@@ -57,17 +60,115 @@ struct ContentView: View {
                                 }
                             }
                     )
+                    //MARK: Magnification Gesture
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    } else if imageScale < 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                    )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
                 isAnimating = true
             }
+            //MARK: Info Panel
             .overlay (
                 InfoPanelView(scale: imageScale, offset: imageOffset)
                     .padding(.horizontal)
                     .padding(.top, 30)
-                ,alignment: .top
+                , alignment: .top
+            )
+            //MARK: Controls
+            .overlay(
+                Group {
+                    HStack {
+                        //MARK: Scale Down
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+
+                        
+                        //MARK: Reset
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        
+                        //MARK: Scale Up
+                        Button {
+                            if imageScale < 5 {
+                                imageScale += 1
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                    }
+                    .padding(.init(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(8)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.easeOut(duration: 1), value: isAnimating)
+                }
+                    .padding(.bottom, 30)
+                , alignment: .bottom
+            )
+            //MARK: Drawer
+            .overlay(
+                HStack(spacing: 12) {
+                    //MARK: Drawer Handle
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        }
+                    
+                    //MARK: Thumbnails
+                    Spacer()
+                } //: Drawer
+                .padding(.init(top: 16, leading: 8, bottom: 16, trailing: 8))
+                .background(.ultraThinMaterial)
+                .cornerRadius(12)
+                .opacity(isAnimating ? 1 : 0)
+                .frame(width: 260)
+                .padding(.top, UIScreen.main.bounds.height / 12)
+                .offset(x: isDrawerOpen ? 20 : 215)
+                , alignment: .topTrailing
             )
         }
         .navigationViewStyle(.stack)
